92078623ea124b95e9f0f184f11c0375ce2ca6bf
Jose-Prince <joseprince148@gmail.com>
2023-09-09 20:55:11 -0600

    Feat: Generate swimmers's velocities

--- WhirpoolSimulation.cpp
+++ WhirpoolSimulation.cpp
@@ -14,13 +14,61 @@
 * --------------------------------------------------------*/
 
 #include <iostream>
+#include <omp.h>
+#include <random>
+#include <cstdlib>
+#include <time.h>
+using namespace std;
 
+float randomFloat()
+{
+    return (float)(rand()) / (float)(RAND_MAX);
+}
 
+int randomInt(int a, int b)
+{
+    if (a > b)
+        return randomInt(b, a);
+    if (a == b)
+        return a;
+    return a + (rand() % (b - a));
+    
+}
+
+float randomFloat(int a, int b)
+{
+    if (a > b)
+        return randomFloat(b, a);
+    if (a == b)
+        return a;
+    
+    return (float)randomInt(a, b) + randomFloat();
+}
 
 main() {
-    int distancia;
-    
-    std::cout << "Ingrese la distancia total a nadar (50/100/200/400 m): ";
-    std::cin >> distancia;
 
+    srand((unsigned) time(NULL));
+
+    int distance; //Distance of the competition
+
+    float swimmers[8][4]; //Rows --> Swimmer    Column --> Swimming style
+
+    std::cout << "Ingrese la distancia total a nadar (50/100/200/400 m): "; 
+    std::cin >> distance; //Scan for distance swum 
+
+    #pragma omp parallel
+    {
+            
+        #pragma omp parallel for  
+        for (int i = 0; i < 8; i++)
+        {
+            for (int j = 0; j < 4; j++)
+            {
+                swimmers[i][j] = randomFloat(1, 3); //Generate a random float number beetween 1 and 3
+            }  
+        }        
+    }
+    
+    std::cout << std::endl;
 }
+
