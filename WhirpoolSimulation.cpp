0ea565c8136a48bda1184c34ca8a43a100d92562
Jose-Prince <joseprince148@gmail.com>
2023-09-09 19:15:59 -0600

    Feat: Input distance

---
+++ WhirpoolSimulation.cpp
@@ -0,0 +1,26 @@
+/*--------------------------------------------------------
+* UNIVERSIDAD DEL VALLE DE GUATEMALA
+* FACULTAD DE INGENIERÍA
+* DEPARTAMENTO DE CIENCIA DE LA COMPUTACIÓN
+* CC3086 Programación de microprocesadores
+* Proyecto 2: Simulación de competencia de natación
+* 
+* WhirpoolSimulation.cpp
+* Original: 
+* Creado por: José Prince, Andre Jo, Irving Acosta y Daniel RAyo
+* Fecha: 21/09/2023
+* Descripción: Simulador de competencia de natación 
+* para diferentes estilos
+* --------------------------------------------------------*/
+
+#include <iostream>
+
+
+
+main() {
+    int distancia;
+    
+    std::cout << "Ingrese la distancia total a nadar (50/100/200/400 m): ";
+    std::cin >> distancia;
+
+}
