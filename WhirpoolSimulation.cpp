88afb6c6d04da5693d1c51a9263f605623ae1cb3
Daniel Rayo <danirayoroldan@gmail.com>
2023-09-14 18:15:18 -0600

    Feat: Fix swimmers array, and implemen swimmers live printing

--- WhirpoolSimulation.cpp
+++ WhirpoolSimulation.cpp
@@ -13,52 +13,118 @@
 * para diferentes estilos
 * --------------------------------------------------------*/
 
+#include "randomGenerator.h" // importing random generator funtions
+
+#if __has_include(<unistd.h>) // Importing sleep() for different OS
+#endif
+#if __has_include(<windows.h>)
+#include <unistd.h>
+#endif
+
+#include <cstdio>
 #include <iostream>
 #include <omp.h>
 #include <random>
 #include <cstdlib>
 #include <time.h>
-#include "randomGenerator.h" // importing random generator funtions
+#include <unistd.h>
 using namespace std;
 
+#define PRINT_WIDTH 40
+#define NUM_SWIMMERS 8
+
+void printSwimmerPositions(int len, float position);
+void clearScreen();
+
 int main() {
+
+// #######################################
+// ###  INITALIZIN SWIMMERS PARAMETERS ###
+// #######################################
 
     srand((unsigned) time(NULL)); //Seed Generator
 
     int distance; //Distance of the competition
     int estilo;
 
-    float swimmers[8][4]; //Rows --> Swimmer    Column --> Swimming style
-                            //Swimming Styles: 0 -> Estilo libre, 1 -> Dorso, 2 -> Pecho, 3 -> Mariposa
+    float swimmers[NUM_SWIMMERS][2]; //Rows --> Swimmer    Column1 --> Swimming style Column2 --> position
+                                    //Swimming Styles: 0 -> Estilo libre, 1 -> Dorso, 2 -> Pecho, 3 -> Mariposa
 
     std::cout << "Ingrese la distancia total a nadar (50/100/200/400 m): "; 
     std::cin >> distance; //Scan for distance to swim 
-
-    #pragma omp parallel
-    {
-            
-        #pragma omp parallel for  
-        for (int i = 0; i < 8; i++)
-        {
-            for (int j = 0; j < 4; j++)
-            {
-                swimmers[i][j] = randomFloat(1, 3); //Generate a random float number beetween 1 and 3
-            }  
-        }        
-    }
-
+                          //
     printf("\n\nEstilos de nado:\n1: Estilo libre\n2: Dorso\n3: Pecho\n4: Mariposa\n");
     std::cout << "Ingrese el estilo de nado (1 - 4): "; 
     std::cin >> estilo; //Scan for swimming style 
 
-    printf("Los nadadores tienen las siguientes velocidades: ");
-    for (int i = 0; i < 8; i++)
+    #pragma omp parallel
     {
-        printf("\nNadador %d: %f", i+1,swimmers[i][estilo-1]);
+        #pragma omp parallel for  
+        for (int i = 0; i < NUM_SWIMMERS; i++)
+        {
+            swimmers[i][0] = randomFloat(1, 3); //Generate a random float number beetween 1 and 3
+            swimmers[i][1] = 0; // Set initial position
+        }        
     }
-    
-    
+
+    printf("Los nadadores tienen las siguientes velocidades (m/s): ");
+    for (int i = 0; i < NUM_SWIMMERS; i++)
+    {
+        printf("\nNadador %d: %f", i+1,swimmers[i][0]);
+    }
     std::cout << std::endl;
+    
+// ####################
+// ###  MAIN LOOP   ###
+// ####################
+
+    // TODO: Andre, change the condition to stop loop when one swimmer win.
+    while (true){
+        // TODO: Andre must create the process to define future swimmers positions 
+
+        // ## PRINTING SWIMMERS POSITIONS ##
+        for(int i = 0; i < NUM_SWIMMERS; i++){
+            for(int j = 0; j < PRINT_WIDTH; j++){
+                printf("#");
+            }
+            printf("\n");
+            printSwimmerPositions(distance, swimmers[i][1]);
+        }
+        sleep(2);
+        clearScreen();
+        break;
+    }
 
     return 0;
 }
+
+// Prints a representation of swimmer on its rale.
+void printSwimmerPositions(int distance, float position){
+
+    int positionInBar = (position * PRINT_WIDTH) / distance;
+
+    for(int i = 0; i < PRINT_WIDTH; i++){
+        if(i == positionInBar)
+            printf("\033[1;30;41mo\033[0m"); // Print Swimmer
+        else if (i < positionInBar)
+            printf("=");                    // Print space swimmed
+        else
+            printf("-");                    // Print remaining space
+    }
+    printf("\n");
+}
+
+// Clear terminal screen.
+void clearScreen()
+{
+#if defined _WIN32
+    system("cls");
+    //clrscr(); // including header file : conio.h
+#elif defined (__LINUX__) || defined(__gnu_linux__) || defined(__linux__)
+    system("clear");
+    //std::cout<< u8"\033[2J\033[1;1H"; //Using ANSI Escape Sequences 
+#elif defined (__APPLE__)
+    system("clear");
+#endif
+}
+
